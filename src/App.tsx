14b02d6a421b81e5ed89ef19e032485c1709d6da
Ansh Kumar <ansh.km7096@gmail.com>
2025-04-11 00:21:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import EmailSender from './EmailSender';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-[#0B192C] py-8">
       <div className="container mx-auto px-4">
         <EmailSender />
       </div>
